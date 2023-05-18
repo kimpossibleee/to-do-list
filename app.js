d0d434b94f6282d2b679db3f7b7ccad03a0de850
kimpossibleee <yeayoungk@gmail.com>
2023-05-18 02:06:46 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,35 @@
+function addTask() {
+    let input = document.getElementById("taskInput");
+    let task = input.value;
+    if (task === "") {
+      return;
+    }
+
+    let li = document.createElement("li");
+    let checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.addEventListener("change", function () {
+      if (checkbox.checked) {
+        li.classList.add("completed");
+      } else {
+        li.classList.remove("completed");
+      }
+    });
+
+    let label = document.createElement("label");
+    label.innerHTML = task;
+
+    let deleteBtn = document.createElement("button");
+    deleteBtn.innerHTML = "Delete";
+    deleteBtn.classList.add("delete-btn");
+    deleteBtn.addEventListener("click", function () {
+      li.parentNode.removeChild(li);
+    });
+
+    li.appendChild(checkbox);
+    li.appendChild(label);
+    li.appendChild(deleteBtn);
+    document.getElementById("taskList").appendChild(li);
+
+    input.value = "";
+  }
